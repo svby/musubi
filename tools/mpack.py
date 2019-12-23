921802f86f9d1e3272e24c039322f9bc1a5dad95
stuhlmeier <stuhlmeier@gmail.com>
2019-12-23 15:14:56 +0100

    Add support to mpack for (recursively) packing multiple directories at once
    Signed-off-by: stuhlmeier <stuhlmeier@gmail.com>

--- tools/mpack.py
+++ tools/mpack.py
@@ -29,9 +29,14 @@ class MPack:
     def __init__(self, args: argparse.Namespace) -> None:
         self.is_verbose = args.verbose
         self.output = args.output
-        self.file = args.file
+        self.recursive = args.recursive
+        self.files = args.files
+
+        self.single = len(self.files) == 1
 
     def pack_from_meta(self, name: Optional[str], meta_path: Path, destination_parent: Path) -> int:
+        self.info(f"packing {meta_path}")
+
         parent_path = meta_path.parent
 
         try:
@@ -41,15 +46,18 @@ class MPack:
             self.error(f"{meta_path}: metadata file does not exist")
             return -1
 
-        name = name or meta.get("name", "pack")
+        name = name or meta.get("name", meta_path.parent.name)
 
         if destination_parent.is_dir():
             destination_path = destination_parent / f"{name}.mpack"
-        else:
+        elif self.single:
             if destination_parent.suffix != ".mpack":
                 destination_path = destination_parent.parent / f"{destination_parent.name}.mpack"
             else:
                 destination_path = destination_parent
+        else:
+            self.error(f"{destination_parent}: destination is not a directory")
+            return -1
 
         try:
             tar = tarfile.open(destination_path, "w:xz", dereference=True)
@@ -83,26 +91,44 @@ class MPack:
         return 0
 
     def pack(self) -> int:
-        path = Path(self.file).resolve()
-        self.info(f"processing {path}")
+        for file in self.files:
+            path = Path(file).resolve()
+            self.info(f"processing {path}")
 
-        if path.is_dir():
-            self.verbose(f"{path}: is a directory")
-            return self.pack_from_meta(
-                path.name,
-                path / "pack.json",
-                Path(self.output or os.getcwd())
-            )
-        elif path.exists():
-            self.verbose(f"{path}: is a file")
-            return self.pack_from_meta(
-                None,
-                path,
-                Path(self.output or os.getcwd())
-            )
-        else:
-            self.error(f"{path}: does not exist")
-            return -1
+            if path.is_dir():
+                self.verbose(f"{path}: is a directory")
+                if not self.recursive:
+                    status = self.pack_from_meta(
+                        path.name,
+                        path / "pack.json",
+                        Path(self.output or os.getcwd())
+                    )
+                    if status != 0:
+                        return status
+                else:
+                    for root, _, files in os.walk(path, topdown=False):
+                        if "pack.json" in files:
+                            path = Path(root) / "pack.json"
+                            self.verbose(f"{path}: is a file")
+                            status = self.pack_from_meta(
+                                None,
+                                path,
+                                Path(self.output or os.getcwd())
+                            )
+                            if status != 0:
+                                return status
+            elif path.exists():
+                self.verbose(f"{path}: is a file")
+                status = self.pack_from_meta(
+                    None,
+                    path,
+                    Path(self.output or os.getcwd())
+                )
+                if status != 0:
+                    return status
+            else:
+                self.error(f"{path}: does not exist")
+                return -1
 
 
 def main() -> None:
@@ -113,11 +139,16 @@ def main() -> None:
     )
     parser.add_argument(
         "--output", "-o", action="store",
-        help="destination file name"
+        help="the output filename or parent directory if a single file is specified, "
+             "or the output directory for all packs"
     )
     parser.add_argument(
-        "file", type=str,
-        help="a directory or pack.json file"
+        "--recursive", "--recur", "-r", action="store_true",
+        help="recursively search all specified directories for pack.json files"
+    )
+    parser.add_argument(
+        "files", type=str, nargs="+",
+        help="a list of directories or pack.json files"
     )
 
     status = MPack(parser.parse_args()).pack()
